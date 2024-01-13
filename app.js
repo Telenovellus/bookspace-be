0096b50867b2073eca04bd8d2e9ea8d54d5cbb6a
Indra <indraahmadfirdaus@gmail.com>
2024-01-13 15:30:23 +0700

    fix env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { config: envConfig } = require('dotenv');
 
 
 // config and middlewares
-envConfig()
+if(process.env.ENV !== 'PRODUCTION') envConfig()
 connectDb();
 app.use(morgan('dev'));
 app.use(cors());
